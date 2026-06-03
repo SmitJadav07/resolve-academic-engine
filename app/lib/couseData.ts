e30ebe7a31e4507f5d92957f2738e40c1a769a4f
Smit Jadav <smitcjadav07@gmail.com>
2026-06-02 20:14:36 -0400

    Create initial course graph

---
+++ app/lib/couseData.ts
@@ -0,0 +1,22 @@
+export const courses = [
+  {
+    code: "CS100",
+    prerequisites: [],
+  },
+  {
+    code: "CS113",
+    prerequisites: ["CS100"],
+  },
+  {
+    code: "CS114",
+    prerequisites: ["CS113"],
+  },
+  {
+    code: "CS241",
+    prerequisites: ["CS114"],
+  },
+  {
+    code: "CS280",
+    prerequisites: ["CS241"],
+  },
+];
